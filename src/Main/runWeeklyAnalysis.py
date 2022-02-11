d5bba9408cc01f883ae139d2a2781942624433d6
lau-bensonnh <lau.bensonnh@hotmail.com>
2022-02-11 17:57:07 +0800

    2022-02-11 Update Weekly Analysis

--- src/Main/runWeeklyAnalysis.py
+++ src/Main/runWeeklyAnalysis.py
@@ -5,12 +5,17 @@ import psycopg2
 # from src.DataSource.GetHistData = Get Daily Historical Data
 from src.DataSource.GetHistData import initial_weekly_data
 # from src.Calculation.ComputeMACD
-from src.Calculation.ComputeMACD import computeMACD
+from src.Calculation.ComputeMACD import compute_macd
+from src.Calculation.ComputeMACD import check_macd
+
 # from src.Calculation.STC
 from src.Calculation.ComputeSTC import compute_stc
+from src.Calculation.ComputeSTC import check_stc
 
 
 ticker = "9988.hk"
+
+# Grab Data
 data = initial_weekly_data(ticker)
 print(data)
 
@@ -23,13 +28,15 @@ print(data)
 data['market_date'] = pd.to_datetime(data['market_date'])
 
 # Calculate MACD
-data = computeMACD(data, 12, 26, 9)
+data = compute_macd(data, 12, 26, 9)
 print(data)
+
+# Check MACD Status
+momentumCheck = check_macd(data)
 
 # Calculate STC
 data = compute_stc(data, 16, 8, 8)
 print(data)
 
-
-
-
+# Check STC Status
+stcCheck = check_stc(data)
